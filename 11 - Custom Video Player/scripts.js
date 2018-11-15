612450d858b6751c273ae13f86e895029e793fa4
Carlos <cpacheco4983@gmail.com>
2018-11-15 15:50:45 -0500

    Completed day 11. WIP: fullscreen

---
+++ 11 - Custom Video Player/scripts.js
@@ -0,0 +1,65 @@
+// Get Elements
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progressBar = player.querySelector('.progress');
+const progress = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
+const skipBtns = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
+
+const fullscreenBtn = player.querySelector('.fullscreen');
+
+// Build functions
+function togglePlay() {
+  video.paused ? video.play() : video.pause();
+}
+
+function updateBtn() {
+  const icon = this.paused ? '►' : '❚❚';
+  toggle.textContent = icon;
+}
+
+function skip() {
+  video.currentTime += parseFloat(this.dataset.skip);
+}
+
+function handleRangeUpdate() {
+  video[this.name] = this.value;
+}
+
+function handleProgress() {
+  const percent = (video.currentTime / video.duration) * 100;
+  progress.style.flexBasis = `${percent}%`;
+}
+
+function scrub(e) {
+  const scrubTime = (e.offsetX / progressBar.offsetWidth) * video.duration;
+  video.currentTime = scrubTime;
+}
+
+// function toggleFullscreen() {
+//   console.log('btnclick');
+//   player.webkitRequestFullscreen();
+// }
+
+// Event LIsteners
+video.addEventListener('click', togglePlay);
+// We update the button outside of the click event because video will not always pause on video/btn click
+video.addEventListener('play', updateBtn);
+video.addEventListener('pause', updateBtn);
+video.addEventListener('timeupdate', handleProgress);
+
+toggle.addEventListener('click', togglePlay);
+
+skipBtns.forEach(btn => btn.addEventListener('click', skip));
+
+ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
+ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
+
+let mousedown = false;
+progressBar.addEventListener('click', scrub);
+progressBar.addEventListener('mousemove', (e) => mousedown && scrub(e));
+progressBar.addEventListener('mousedown', () => mousedown = true);
+progressBar.addEventListener('mouseup', () => mousedown = false);
+
+// fullscreenBtn.addEventListener('click', toggleFullscreen);
